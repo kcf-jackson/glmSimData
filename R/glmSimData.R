7933a0f9ba612508546740beb4672397b082af07
kcf-jackson <kcf.jackson@gmail.com>
2016-07-06 13:17:27 +1000

    Minor update. - Renamed package name from "artificialData" to "glmSimData". - Updated documentation.

--- R/glmSimData.R
+++ R/glmSimData.R
@@ -1,7 +1,7 @@
 #' A package for simulating data
 #' @description This package simulates artifical data following a GLM model with dependent / independent, numerical / ordinal covariates.
 #' @docType package
-#' @name artificialData
+#' @name glmSimData
 #' @author Jackson Kwok
 NULL
 #> NULL
