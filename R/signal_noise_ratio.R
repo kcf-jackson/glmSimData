b15340d35fb200a249deb51de9935d825dd0d43a
kcf-jackson <kcf.jackson@gmail.com>
2016-07-10 22:42:30 +1000

    Add a function to draw signals given labels

--- R/signal_noise_ratio.R
+++ R/signal_noise_ratio.R
@@ -9,9 +9,9 @@ init_signal_noise <- function(my_data, family, tf = identity) {
 
 #' extract signal-noise ratio from GLM object.
 #' @param model_obj 'glm' object.
-#' @param absolute boolean; if FALSE, return the signed ratio. 
-#' @details The signal-noise ratio must be non-negative, but sometimes the 
-#' signs are needed for analysis. 
+#' @param absolute boolean; if FALSE, return the signed ratio.
+#' @details The signal-noise ratio must be non-negative, but sometimes the
+#' signs are needed for analysis.
 #' @export
 extract_ratio <- function(model_obj, absolute = TRUE) {
   coeff <- summary(model_obj)$coefficients
@@ -35,12 +35,12 @@ diff_ratio <- function(r1, r2) {
 
 
 #' Categorise signal-noise ratio
-find_signal_label <- function(signal_noise_ratio) {
+atomic_find_signal_label <- function(signal_noise_ratio) {
   # <--XS-- 0.22 --S-- 0.61 --M-- 1.65 --L-- 4.48 --XL-->
   # SN_intervals <- c(0.22, 0.61, 1.65, 4.48)  #on exponential scale
   # SN_labels <- c("XS", "S", "M", "L", "XL")
   # <--S-- 1.65 --M-- 4.48 --L-->
-  SN_intervals <- c(1.65, 4.48)  #on exponential scale
+  SN_intervals <- exp(c(0.5, 1.5))
   SN_labels <- c("S", "M", "L")
   for (i in seq_along(SN_intervals)) {
     if (signal_noise_ratio < SN_intervals[i])
@@ -49,10 +49,10 @@ find_signal_label <- function(signal_noise_ratio) {
   tail(SN_labels, 1)
 }
 #' Categorise signal-noise ratio (vectorised)
-#' @param signal_noise_ratio numeric vector; 
+#' @param signal_noise_ratio numeric vector;
 #' @return character vector; the labels.
 #' @export
-vec_find_signal_label <- Vectorize(find_signal_label)  #Vectorize
+find_signal_label <- Vectorize(atomic_find_signal_label)  #Vectorize
 
 
 #' Generate some random labels
@@ -62,4 +62,41 @@ vec_find_signal_label <- Vectorize(find_signal_label)  #Vectorize
 #' @export
 random_labels <- function(num_label, labels = c("S", "M", "L")) {
   sample(labels, num_label, replace = TRUE)
+}
+
+
+#' Generate some random signals based on labels
+#' @description Given a label, the function draws a number from the interval
+#' corresponding to the label.
+#' @param labels vector; the desired labels
+#' @param labels_table dataframe; a table mapping labels to intervals.
+#' See `create_labels_table()` for an example.
+#' @examples
+#' labels_table <- create_labels_table()
+#' random_signals(c(rep("S", 4), rep("M", 3)), labels_table)
+#' @export
+random_signals <- function(labels, labels_table) {
+  if (!all(labels %in% labels_table$labels))
+    stop("Not all labels are mapped to an interval.")
+  purrr::map_dbl(labels, ~draw_signal(.x, labels_table))
+}
+draw_signal <- function(label, labels_table) {
+  ind <- which(labels_table$labels == label)
+  mapping <- labels_table[ind, ]
+  if (mapping$max == Inf)
+    return(mapping$min + rexp(1))
+  runif(1, min = mapping$min, max = mapping$max)
+}
+
+
+#' Create a dataframe mapping labels to intervals.
+#' @param labels vector; the labels.
+#' @param partition vector; the breakpoint partitioning the positive real line.
+#' @export
+create_labels_table <- function(labels = c("S", "M", "L"),
+                                partition = c(0, exp(c(0.5, 1.5)), Inf)) {
+  labels <- unique(labels)
+  data.frame(labels = labels,
+             min = head(partition, -1),
+             max = tail(partition, -1))
 }
