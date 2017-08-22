8e0935d35df655aa4befefb552e59eb844755c17
kcf-jackson <kcf.jackson@gmail.com>
2017-08-22 12:33:17 +1000

    Modify functions' default behaviour

--- R/features_independent.R
+++ R/features_independent.R
@@ -8,11 +8,13 @@
 #' for more details on the extra parameters. If 'discrete_uniform' is chosen, one
 #' can specify the distribution for the categories through the parameter 'prob'.
 #' @export
-generate_features <- function(n, p, distn = "gaussian", num_categories = 5, ...) {
+generate_features <- function(n, p, distn = "gaussian", num_categories, ...) {
   distn_list <- c("gaussian", "student-t", "pareto", "uniform",
                   "discrete_uniform", "poisson", "gamma")
-  if ((distn == 'discrete_uniform') & (missing(num_categories)))
+  if ((distn == 'discrete_uniform') && (missing(num_categories))) {
+    num_categories <- 5
     print("parameter 'num_categories' is not provided, it is set to 5 by default.")
+  }
   if (!(distn %in% distn_list))
     stop(
       paste(c("Must be one of '", paste(distn_list, collapse = "', '"), "'.")),
@@ -23,7 +25,7 @@ generate_features <- function(n, p, distn = "gaussian", num_categories = 5, ...)
     # Categorical distribution
     samples <- sample(x = num_categories, size = num_sim, replace = TRUE, ...) - 1
     res <- data.frame(matrix(samples, nrow = n, ncol = p))
-    res <- purrr::map(res, as.factor) %>% data.frame()
+    # res <- purrr::map(res, as.factor) %>% data.frame()  # turn into factors
   } else {
     # Numerical distribution
     rdist <- switch(distn,
--- R/features_util.R
+++ R/features_util.R
@@ -2,14 +2,15 @@
 #' @param df0 dataframe; the data
 #' @param p integer; number of irrelevant covariates to be added
 #' @param distn One of 'gaussian', 'student-t', 'pareto', 'uniform', 'discrete_uniform', 'poisson', 'gamma'.
+#' @param num_categories integer; number of categories for 'discrete_uniform'.
 #' @param ... Optional parameters to be passed to "generate_features".
 #' @details This function calls 'generate_features'.
 #' See '?generate_features' for details.
 #' @export
-add_irrelevant_features <- function(df0, p, distn, ...) {
+add_irrelevant_features <- function(df0, p, distn, num_categories, ...) {
   #can change to dependent easily by converting to 'generate_dependent_covaraites' and
   #then subset the 'covariates' component.
-  irrelevant_features <- generate_features(nrow(df0), p, distn, ...)
+  irrelevant_features <- generate_features(nrow(df0), p, distn, num_categories, ...)
   names(irrelevant_features) <- paste( 'irr_', seq(p), sep = "" )
   dplyr::bind_cols(df0, irrelevant_features)
 }
